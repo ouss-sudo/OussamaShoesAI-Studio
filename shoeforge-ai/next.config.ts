2edd4b962584283280da7414e64dbc8f9aca7120
oussama ben hamida <80219144+ouss-sudo@users.noreply.github.com>
2026-03-30 00:53:13 +0200

    update modifications

--- shoeforge-ai/next.config.ts
+++ shoeforge-ai/next.config.ts
@@ -1,6 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: 'export',
+  // On récupère OussamaShoesAI-Studio de l'URL Github pour permettre un chargement correct.
+  basePath: process.env.NODE_ENV === 'production' ? '/OussamaShoesAI-Studio' : '',
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
 
